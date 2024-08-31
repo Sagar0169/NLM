ef6862bd833a4b8abfe2a58e4087b08446a76156
Shreyash <shreyashjain33@gmail.com>
2024-08-31 16:15:46 +0530

    NLSIA Forms done

---
+++ app/src/main/java/com/nlm/ui/activity/National_Live_Stock_IA.kt
@@ -0,0 +1,21 @@
+package com.nlm.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nlm.R
+
+class National_Live_Stock_IA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_national_live_stock_ia)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
