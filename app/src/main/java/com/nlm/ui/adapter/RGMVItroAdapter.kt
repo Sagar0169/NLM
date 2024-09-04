9a37db2b6123cdf6e7eb4cf255e1cb10de0785f3
Sagar0169 <sagarsttl888@gmail.com>
2024-09-04 11:50:33 +0530

    Vitro fertilization

---
+++ app/src/main/java/com/nlm/ui/adapter/RGMVItroAdapter.kt
@@ -0,0 +1,78 @@
+package com.nlm.ui.adapter
+
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.nlm.R
+import com.nlm.model.OnlyCreated
+import com.nlm.model.RGMVitro
+import com.nlm.ui.activity.AddNewMobileVeterinaryUnit
+import com.nlm.ui.activity.AddRGMVitroFertilizatonActivity
+import com.nlm.utilities.showView
+
+
+class RGMVItroAdapter(private val onlyCreated: List<RGMVitro>, private val isFrom: Int) :
+    RecyclerView.Adapter<RGMVItroAdapter.RGMVItroAdapterViewholder>() {
+
+    // ViewHolder class to hold the view elements
+    class RGMVItroAdapterViewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvStateName: TextView = itemView.findViewById(R.id.tvState)
+        val tvCreated: TextView = itemView.findViewById(R.id.tvCreated)
+        val tvStatus: TextView = itemView.findViewById(R.id.tvStatusData)
+        val ivView: ImageView = itemView.findViewById(R.id.ivView)
+        val ivEdit: ImageView = itemView.findViewById(R.id.ivEdit)
+    }
+
+
+    // Inflate the item layout and create the holder
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RGMVItroAdapterViewholder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_rgm_vitro, parent, false)
+        return RGMVItroAdapterViewholder(itemView)
+    }
+
+    // Bind the data to the views in each item
+    override fun onBindViewHolder(holder: RGMVItroAdapterViewholder, position: Int) {
+        val item = onlyCreated[position]
+        holder.tvStateName.text = item.state
+        holder.tvCreated.text = item.created
+        holder.tvStatus.text = item.status
+
+
+        holder.ivView.setOnClickListener {
+            val intent = Intent(
+                holder.itemView.context,
+                AddRGMVitroFertilizatonActivity::class.java
+            ).putExtra("isFrom", isFrom)
+            holder.itemView.context.startActivity(intent)
+        }
+        holder.ivEdit.setOnClickListener {
+            val intent = Intent(
+                holder.itemView.context,
+                AddRGMVitroFertilizatonActivity::class.java
+            ).putExtra("isFrom", isFrom)
+            holder.itemView.context.startActivity(intent)
+        }
+//
+//        holder.itemView.setOnClickListener {
+//            val intent = Intent(holder.itemView.context, NodalOfficerDetailActivity::class.java)
+//            intent.putExtra("nodalOfficer", item)
+//            holder.itemView.context.startActivity(intent)
+//        }
+
+    }
+
+    // Return the total number of items
+    override fun getItemCount(): Int {
+        return onlyCreated.size
+    }
+}
---
+++ app/src/main/java/com/nlm/ui/fragment/VitroPartTwoFragment.kt
@@ -0,0 +1,91 @@
+package com.nlm.ui.fragment
+
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.nlm.R
+import com.nlm.databinding.FragmentVitroPartTwoBinding
+import com.nlm.ui.adapter.BottomSheetAdapter
+import com.nlm.utilities.BaseFragment
+
+
+class VitroPartTwoFragment : BaseFragment<FragmentVitroPartTwoBinding>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_vitro_part_two
+    private var mBinding: FragmentVitroPartTwoBinding? = null
+    private lateinit var bottomSheetAdapter: BottomSheetAdapter
+    private lateinit var bottomSheetDialog: BottomSheetDialog
+
+    private val state = listOf(
+        "Left Artificial", "Right Artificial", "Left Squint", "Right Squint", "Others"
+    )
+
+    private val district = listOf(
+        "Black", "Brown", "Blue", "Reddish", "Green", "Other"
+    )
+
+    override fun init() {
+        mBinding = viewDataBinding
+        mBinding?.clickAction = ClickActions()
+    }
+
+    override fun setVariables() {
+
+    }
+
+    override fun setObservers() {
+
+    }
+
+    inner class ClickActions {
+//        fun state(view: View){showBottomSheetDialog("state")}
+//        fun district(view: View){showBottomSheetDialog("district")}
+    }
+//    private fun showBottomSheetDialog(type: String) {
+//        bottomSheetDialog = BottomSheetDialog(requireContext())
+//        val view = layoutInflater.inflate(R.layout.bottom_sheet_state, null)
+//        view.layoutParams = ViewGroup.LayoutParams(
+//            ViewGroup.LayoutParams.MATCH_PARENT,
+//            ViewGroup.LayoutParams.WRAP_CONTENT
+//        )
+//
+//        val rvBottomSheet = view.findViewById<RecyclerView>(R.id.rvBottomSheet)
+//        val close = view.findViewById<TextView>(R.id.tvClose)
+//
+//        close.setOnClickListener {
+//            bottomSheetDialog.dismiss()
+//        }
+//
+//        // Define a variable for the selected list and TextView
+//        val selectedList: List<String>
+//        val selectedTextView: TextView
+//
+//        // Initialize based on type
+//        when (type) {
+//
+//
+//            "state" -> {
+//                selectedList = state
+//                selectedTextView = mBinding!!.etState
+//            }
+//
+//            "district" -> {
+//                selectedList = district
+//                selectedTextView = mBinding!!.etDistrict
+//            }
+//
+//
+//
+//            else -> return
+//        }
+//        bottomSheetAdapter = BottomSheetAdapter(requireContext(),selectedList) { selectedItem ->
+//            selectedTextView.text = selectedItem
+//            bottomSheetDialog.dismiss()
+//        }
+//
+//        rvBottomSheet.layoutManager =
+//            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+//        rvBottomSheet.adapter = bottomSheetAdapter
+//        bottomSheetDialog.setContentView(view)
+//
+//        bottomSheetDialog.show()
+//    }
+}
